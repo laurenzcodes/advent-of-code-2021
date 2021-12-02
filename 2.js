b871e5e0e5ac0112fa1c0f698998aa13df9b9698
Robert Marquardt <laurenz1337@gmail.com>
2021-12-02 23:44:29 +0100

    Added solution to part 2 of day 2

--- 2.js
+++ 2.js
@@ -19,6 +19,29 @@ function computeLocation(step) {
     return posX * posY;
 }
 
+// Part 2
+function computeLocationAccurate(step) {
+    let posX = 0;
+    let posY = 0;
+    let aim = 0;
+    for ( let i = 0; i < step.length; i++ ) {
+        const command = step[i].split(' ')[0];
+        const amount = parseInt(step[i].split(' ')[1]);
+        switch (command) {
+            case 'forward':
+                posX += amount;
+                posY += aim * amount
+                break;
+            case 'down':
+                aim += amount;
+                break;
+            case 'up':
+                aim -= amount;
+        }
+    }
+    return posX * posY;
+}
+
 // Get values from DOM and output result
 $( document ).ready( function () {
     $( "#input-day2" ).on( "input", function () {
@@ -27,6 +50,6 @@ $( document ).ready( function () {
 
         $( "#output-day2-1" ).text( computeLocation(inputArray) )
 
-        $( "#output-day2-2" ).text(  )
+        $( "#output-day2-2" ).text( computeLocationAccurate(inputArray) )
     } )
 } )
